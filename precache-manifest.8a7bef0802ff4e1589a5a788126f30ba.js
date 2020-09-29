cfb0e3c8ec89fd5660d154301d71e792f8216b7a
Mythireyan <mythireyanp@gmail.com>
2020-09-29 13:42:02 +0530

    Updates

---
+++ precache-manifest.8a7bef0802ff4e1589a5a788126f30ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff12a60ae834db324e24b78620a14a7",
+    "url": "/face-identifier/index.html"
+  },
+  {
+    "revision": "10bd2b9b549c2f3aab39",
+    "url": "/face-identifier/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "038ab214d70eab4a458d",
+    "url": "/face-identifier/static/css/main.e7af8a23.chunk.css"
+  },
+  {
+    "revision": "10bd2b9b549c2f3aab39",
+    "url": "/face-identifier/static/js/2.7b69494b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-identifier/static/js/2.7b69494b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038ab214d70eab4a458d",
+    "url": "/face-identifier/static/js/main.79ebcbbc.chunk.js"
+  },
+  {
+    "revision": "5c27d64720502a3102c4",
+    "url": "/face-identifier/static/js/runtime-main.6d67a802.js"
+  }
+]);
